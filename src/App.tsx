f6fe3710eac72c5da53c6d7c54f2ec198e331f2e
Tan Nguyen <duytan1909@gmail.com>
2021-11-25 11:18:11 +0700

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import LocationWeather from "./components/LocationWeather/LocationWeather";
+import SearchBar from "./components/SearchBar/SearchBar";
+import "./styles/main.scss";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: { refetchOnWindowFocus: false, staleTime: 300000 },
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <QueryClientProvider client={queryClient}>
+                <SearchBar />
+                <LocationWeather />
+            </QueryClientProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchBar() {
+    return (
+        <div className="SearchBar app-bar navbar bg-dark">
+            <div className="container py-3"></div>
+        </div>
+    );
+}
